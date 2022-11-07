59245023494be66e25837e6db565addc4322a76f
Rebeca Guijosa <113552170+Rguijosa@users.noreply.github.com>
2022-11-07 20:38:53 +0000

    we combined two functions

--- function_interactions.py
+++ function_interactions.py
@@ -9,15 +9,44 @@
 # def function_2(a):
 # |...
 # |return b
-# def function_3(b):
+# # def function_3(b):
 # |...
 # |return c
 # def function_4(a,c):
 # |...
 # |return d
+from random import shuffle
 
-def interactions():
-  pass
+  # initial list
+sticks=["-","--","---","----"]
+
+def mix(my_list):
+  shuffle(my_list)
+  return(my_list)
+
+print(mix(sticks))
+  # mixing sticks
+def try_your_luck():
+  a_try=""
+  while a_try not in ['1','2','3','4']:
+     a_try= input('choose your number')
+    
+  return int(a_try)
+
+# verify my try
+def verify_my_try(a_list, a_try):
+  if a_list[a_try-1]=='-':
+    print("wash the dishes")
+  else:
+      print("this time you are safe")
+
+  print(f"you got a {a_list[a_try-1]}")
+
+    
+
+
+
+  # choose number
 
 #####################################################################################################
 
--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from methods_help import method_help, greetings
-from functions import function
-from returnStuff import returnS
+# from methods_help import method_help, greetings
+# from functions import function
+# from returnStuff import returnS
+from function_interactions import *
+sticks=["-","--","---","----"]
+my_mix=(mix(sticks))
 
-method_help()
-greetings("thomas")
-greetings("Sam")
-greetings("Rebeca")
+try1= try_your_luck()
+print(try1)
+
+print(verify_my_try(my_mix, try1))
+# method_help()
+# greetings("thomas")
+# greetings("Sam")
+# greetings("Rebeca")
 # function()
-# returnS()
+# returns()
+
